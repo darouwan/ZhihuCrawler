c52478345850d73f8583e6bbd0f44c98e10aa659
Junfeng Chen <k-2feng@hotmail.com>
2015-02-09 17:43:29 +0800

    get followers number

--- grab_web.py
+++ grab_web.py
@@ -15,28 +15,33 @@ profile_ready = [startUrl]
 # print(content)
 
 def discoverProfile(profile_url):
+    print(profile_url)
     # headers = {'Use-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-    #profile_url = "http://www.zhihu.com/people/darouwan-chen"
+    # profile_url = "http://www.zhihu.com/people/darouwan-chen"
     profile_req = urllib.request.Request(profile_url, headers=headers)
     profile_data = urllib.request.urlopen(profile_req).read()
     profile_content = bs4.BeautifulSoup(profile_data)
     profiles = profile_content.find_all('div', 'zm-profile-header-main')
     for profile in profiles:
         names = profile.find_all('span', 'name')
+        gender = ''
         if len(names) > 0:
             name = names[0].contents[0]
 
         maleGenders = profile.find_all('i', 'icon icon-profile-male')
         if len(maleGenders) > 0:
-            print(name, ' is male')
+            gender = 'male'
         else:
             femaleGenders = profile.find_all('i', 'icon icon-profile-female')
             if len(femaleGenders) > 0:
-                print(name, ' is female')
-    discoverNewLinks(profile_content)
+                gender = 'female'
+    discovernewlinks(profile_content)
+    count = getfollowerscount(profile_content)
+
+    print(name, '\t is \t', gender, '\t followers:\t', count)
 
 
-def discoverNewLinks(profile_content):
+def discovernewlinks(profile_content):
     links = profile_content.find_all('a', href=re.compile(r'people'))
     for link in links:
         # print(link.get('href'))
@@ -70,9 +75,22 @@ def discoverNewLinks(profile_content):
             profile_ready.append(rawurl)
 
 
-while len(profile_ready) > 0:
-    for startUrl in profile_ready:
-        discoverProfile(startUrl)
-        print(startUrl)
-        profile_complete.append(startUrl)
-        profile_ready.remove(startUrl)
+def getfollowerscount(profile_content):
+    count = 0
+    followers_sections = profile_content.find_all('a', 'item', href=re.compile(r'/followers$'))
+
+    count = followers_sections[0].find_all('strong')[0].contents[0]
+
+    # print(count)
+    return count
+
+# discoverProfile('http://www.zhihu.com/people/yang-dong-54-6')
+
+if __name__ == '__main__':
+    while len(profile_ready) > 0:
+        for startUrl in profile_ready:
+            discoverProfile(startUrl)
+            profile_complete.append(startUrl)
+            profile_ready.remove(startUrl)
+
+
